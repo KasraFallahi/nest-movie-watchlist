bc9dbaa07728abdf2b38c005003e30db0b4023ec
Kasra Fallahi <kasrafa99@gmail.com>
2023-03-09 20:31:57 +0330

    swagger configs added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './setup-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  setupSwagger(app);
+
+  const port = 3000;
+  await app.listen(port);
+
+  console.info(`server running on ${await app.getUrl()}`);
 }
 bootstrap();

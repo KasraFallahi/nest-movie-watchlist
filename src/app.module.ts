9aec264bc2386f2335666cf8c7ff6c9d22423ac3
Kasra Fallahi <kasrafa99@gmail.com>
2023-03-09 20:13:32 +0330

    typeorm configs added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    //used for environment files
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      entities: ['src/**/*.entity{.ts,.js}'],
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
